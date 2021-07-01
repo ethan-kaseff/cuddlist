c40c31a548ff3710add26963bd012e3dface941c
Ethan Kaseff <ethan@kaseff.com>
2021-07-01 17:53:09 -0500

    string up cuddlist backend route, write update and delete

---
+++ app/api/cuddlist_routes.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required
+from app.models import db, Cuddlist
+from app.forms import CuddlistForm
+
+cuddlist_routes = Blueprint('cuddlists', __name__)
+
+
+@cuddlist_routes.route('/<int:id>', methods=['POST', 'DELETE'])
+@login_required
+def updateProfile():
+    if request.method == 'POST':
+        form = CuddlistForm()
+        cuddlist = Cuddlist.query.get(id)
+
+        cuddlist.first_name = form.data['first_name']
+        cuddlist.last_name = form.data['last_name']
+        cuddlist.pronouns = form.data['pronouns']
+        cuddlist.session_price = form.data['session_price']
+        cuddlist.about_me = form.data['about_me']
+        cuddlist.session_info = form.data['session_info']
+        cuddlist.location = form.data['location']
+
+        db.session.commit()
+
+    elif request.method == 'DELETE':
+        cuddlist = Cuddlist.query.get(id)
+        db.session.delete(cuddlist)
+        db.session.commit()
+
+    return cuddlist.to_dict()
+
---
+++ app/forms/cudlist_form.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Email, ValidationError
+# from app.models import User
+
+
+# def user_exists(form, field):
+#     print("Checking if user exits", field.data)
+#     email = field.data
+#     user = User.query.filter(User.email == email).first()
+#     if user:
+#         raise ValidationError("User is already registered.")
+
+
+class CuddlistForm(FlaskForm):
+    # email = StringField('email', validators=[DataRequired(), user_exists])
+    # password = StringField('password', validators=[DataRequired()])
+    first_name = StringField('first name', validators=[DataRequired()])
+    last_name = StringField('last name', validators=[DataRequired()])
+    pronouns = StringField('pronouns', validators=[])
+    session_price = IntegerField('session price')
+    about_me = TextAreaField('about me')
+    session_info = TextAreaField('session info')
+    location = TextAreaField('location')
