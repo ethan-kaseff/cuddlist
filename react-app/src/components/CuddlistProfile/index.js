2c3f9d1f354f3ab6790206d9ade80ab18e3670f2
Ethan Kaseff <ethan@kaseff.com>
2021-07-05 10:41:37 -0500

    basic component setup

--- react-app/src/components/CuddlistProfile/index.js
+++ react-app/src/components/CuddlistProfile/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 const CuddlistProfile = () => {
+  const {id} = useParams()
+  const cuddlist = useSelector(state => state.cuddlist.current)
+
+  useEffect(() => {
+    dispatch(getCuddlist(id))
+  })
 
   return (
-    
+    <>
+      <div className='cuddlist__container'>
+      </div>
+    </>
   )
 }
 
