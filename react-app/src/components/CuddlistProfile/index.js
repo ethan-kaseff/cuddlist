15b8ecca86dba175b4624d141dae35054b3c4c23
Ethan Kaseff <ethan@kaseff.com>
2021-07-05 11:12:42 -0500

    setup outline for working html in cuddlist profile

--- react-app/src/components/CuddlistProfile/index.js
+++ react-app/src/components/CuddlistProfile/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from 'react-router-dom';
 import { useSelector } from "react-redux";
 
@@ -13,6 +13,24 @@ const CuddlistProfile = () => {
   return (
     <>
       <div className='cuddlist__container'>
+        <div className="cuddlist__container__img__book">
+          <div className="cuddlist__image__carousel">
+
+          </div>
+          <div className="cuddlist__request_session">
+
+          </div>
+        </div>
+        <div className="cuddlist__container__profile__info">
+          <h2 className="cuddlist__name">{cuddlist.firstName}</h2>
+          <p>Session Price: <span>{cuddlist.sessionPrice}</span></p>
+          <p>Travel Price: <span>{cuddlist.travelPrice}</span></p>
+          <p>{cuddlist.location}</p>
+          <h3>About {cuddlist.firstName}</h3>
+          <p className="cuddlist__about__me">{cuddlist.aboutMe}</p>
+          <h3>Session Info</h3>
+          <p className="cuddlist__session__info">{cuddlist.sessionInfo}</p>
+        </div>
       </div>
     </>
   )
