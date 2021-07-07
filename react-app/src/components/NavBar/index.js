5b2a874d512c01dc0c748dd964bfbd241d3fb5f4
Ethan Kaseff <ethan@kaseff.com>
2021-07-07 09:41:01 -0500

    update css

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import LogoutButton from '../auth/LogoutButton';
+
+import './NavBar.css'
 
 const NavBar = () => {
   return (
