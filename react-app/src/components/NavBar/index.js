a1fbdea34c19bf0287438cdc7bb4b8e3020d3d88
Ethan Kaseff <ethan@kaseff.com>
2021-07-09 00:15:54 -0500

    potentially fix heroku logo issue

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import {useSelector} from 'react-redux'
 import LogoutButton from '../auth/LogoutButton';
-import logo from './images/cuddlistlogo2.png';
+import logo from '../../images/cuddlistlogo2.png';
 
 
 const NavBar = () => {
