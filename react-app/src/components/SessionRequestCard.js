bba880b0273aeffe1c37fb6e8a3dce097c9d94fb
Ethan Kaseff <ethan@kaseff.com>
2021-07-09 06:57:51 -0500

    session request styled basically on profile page

--- react-app/src/components/SessionRequestCard.js
+++ react-app/src/components/SessionRequestCard.js
@@ -4,29 +4,58 @@ import {useSelector} from 'react-redux'
 function SessionRequestCard({request}) {
   const user = useSelector(state => state.session.user)
 
+  const date = new Date(request.form.sessionDate)
+  const formattedDate = date.toDateString()
+
   return (
     <div className='p-4'>
       <div className="shadow-xl rounded-lg bg-blue-200 p-3">
         <table className='table-fixed'>
+          <thead>
+            <th className='w-4/12'></th>
+            <th className='w-1/12'></th>
+            <th className='w-7/12'></th>
+          </thead>
           <tbody>
-            <tr>
-              <td className='w-4/12'>
+            <tr className='bg-blue-300 rounded-3xl'>
+              <td>
                 {user.type == 'clients' && 'Cuddlist Name:'}
                 {user.type == 'cuddlists' && 'Client Name:'}
               </td>
-              <td className='w-8/12 pl-2'>
+              <td></td>
+              <td>
                 {user.type == 'clients' && request.cuddler.firstName + ' ' + request.cuddler.lastName}
                 {user.type == 'cuddlists' && request.client.firstName + ' ' + request.client.lastName}
               </td>
             </tr>
             <tr>
               <td>Session Length:</td>
-              <td>{request.sessionLength}</td>
+              <td></td>
+              <td>{request.form.sessionLength}</td>
+            </tr>
+            <tr className='bg-blue-300'>
+              <td>Session Date:</td>
+              <td></td>
+              <td>{formattedDate}</td>
+            </tr>
+            <tr>
+              <td>What would you like to get out of the session?:</td>
+              <td></td>
+              <td>{request.form.getOutOfIt}</td>
+            </tr>
+            <tr className='bg-blue-300 rounded-3xl m-1'>
+              <td>Why are you interested in a cuddle session?:</td>
+              <td></td>
+              <td>{request.form.whySession}</td>
+            </tr>
+            <tr>
+              <td>Do you have any questions for your cuddlist?:</td>
+              <td></td>
+              <td>{request.form.questions}</td>
             </tr>
           </tbody>
         </table>
       </div>
-        <p>{JSON.stringify(request)}</p>
     </div>
   )
 }
