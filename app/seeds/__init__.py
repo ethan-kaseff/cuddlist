2611cc1debfe744ef2e670caaaca1caf7a80b0e5
Ethan Kaseff <ethan@kaseff.com>
2021-07-12 00:17:29 -0500

    put seeds in init file

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .session_requests import seed_session_requests, undo_session_requests
+from .images import seed_images, undo_images
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_session_requests()
+    seed_images()
     # Add other seed functions here
 
 
@@ -19,5 +21,6 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_session_requests()
+    undo_images()
     undo_users()
     # Add other undo functions here
